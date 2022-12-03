74fb3737d334677a22ccaf6453e29539d6ca6c08
Rick Gale <rick.gale@matillion.com>
2022-12-03 16:02:38 +0000

    Fixed app config so it launches again

--- app/src/main/kotlin/com/closeratio/aoc/app/AdventOfCodeApplication.kt
+++ app/src/main/kotlin/com/closeratio/aoc/app/AdventOfCodeApplication.kt
@@ -2,8 +2,10 @@ package com.closeratio.aoc.app
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.context.annotation.ComponentScan
 
 @SpringBootApplication
+@ComponentScan("com.closeratio")
 class AdventOfCode2022Application
 
 fun main(args: Array<String>) {
