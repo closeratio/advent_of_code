51219c3ccf0ccbcd543a169908b49b659d692a50
Rick Gale <rick.gale@matillion.com>
2023-01-02 20:41:25 +0000

    Continued day 19 part 1

--- 2022/day-19/src/main/kotlin/com/closeratio/aoc2022/day19/robot/Robot.kt
+++ 2022/day-19/src/main/kotlin/com/closeratio/aoc2022/day19/robot/Robot.kt
@@ -18,4 +18,39 @@ abstract class Robot(
         )
         .let(::addRobot)
 
+    fun requiredMinutesToBuild(inventory: Inventory): Long? {
+        val requiredResourcesBeingMined = listOf(
+            !(oreCost > 0 && inventory.oreRobots == 0L),
+            !(clayCost > 0 && inventory.clayRobots == 0L),
+            !(obsidianCost > 0 && inventory.obsidianRobots == 0L)
+        ).all { it }
+
+        if (!requiredResourcesBeingMined) {
+            return null
+        }
+
+        return listOf(
+            requiredMinutesForSpecificResource(inventory.oreCount, oreCost, inventory.oreRobots),
+            requiredMinutesForSpecificResource(inventory.clayCount, clayCost, inventory.clayRobots),
+            requiredMinutesForSpecificResource(inventory.obsidianCount, obsidianCost, inventory.obsidianRobots)
+        ).max()
+
+    }
+
+    private fun requiredMinutesForSpecificResource(
+        currentAmount: Long,
+        cost: Long,
+        robotCount: Long
+    ): Long {
+        if (cost == 0L) {
+            return 0
+        } else if (cost <= currentAmount) {
+            return 0
+        }
+
+        val amountToMine = cost - currentAmount
+
+        TODO()
+    }
+
 }
