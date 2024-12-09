9753c44007509d26e439c574ec33a7bc858eeed5
Rick <richardgale90@gmail.com>
2024-12-09 00:42:48 +0000

    Finished 2024 day 7 part 2

--- 2024/day-7/src/main/kotlin/com/closeratio/aoc2024/day7/BridgeCalibrator.kt
+++ 2024/day-7/src/main/kotlin/com/closeratio/aoc2024/day7/BridgeCalibrator.kt
@@ -1,34 +1,27 @@
 package com.closeratio.aoc2024.day7
 
-import org.springframework.core.task.AsyncTaskExecutor
 import org.springframework.stereotype.Service
-import java.util.concurrent.Callable
-import java.util.concurrent.Future
 
 @Service
 class BridgeCalibrator(
-    private val equationSetBuilder: EquationSetBuilder,
-    private val asyncTaskExecutor: AsyncTaskExecutor
+    private val equationSetBuilder: EquationSetBuilder
 ) {
 
     fun calibrate(
         input: List<String>,
         includeConcat: Boolean = false
     ): Long = input
-        .map { line ->
-            asyncTaskExecutor.submit(Callable<Long> {
-                val (testValueStr, termStr) = line.split(": ")
-                val testValue = testValueStr.toLong()
-                val equations = equationSetBuilder.generateEquations(termStr, includeConcat)
+        .mapNotNull { line ->
+            val (testValueStr, termStr) = line.split(": ")
+            val testValue = testValueStr.toLong()
+            val equations = equationSetBuilder.generateEquations(termStr, includeConcat)
 
-                if (equations.any { it.compute() == testValue }) {
-                    testValue
-                } else {
-                    null
-                }
-            })
+            if (equations.any { it.compute() == testValue }) {
+                testValue
+            } else {
+                null
+            }
         }
-        .mapNotNull(Future<Long>::get)
         .sum()
 
 }
