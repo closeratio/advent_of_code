206b2e0668ee04f92053efa37b0bd2dee79be815
RIck Gale <richardgale90@gmail.com>
2025-08-01 20:59:03 +0100

    Fix build issue with missing dirs when migrating to Gradle 9

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,8 +1,17 @@
 rootProject.name = "advent-of-code"
 
+val years = 2015..2024
+val days = 1..25
+
+val projectNames = years.flatMap { year ->
+    days.map { day -> "$year:day-$day" } + "$year:common"
+}
+
 include(listOf(
     "common",
     "app"
-) + (2015..2024).flatMap { year ->
-    (1..25).map { day -> "$year:day-$day" } + "$year:common"
-})
+) + projectNames)
+
+projectNames.forEach {
+    name -> project(":$name").projectDir.mkdirs()
+}
